bbdf17382f0a7c93fa045d72572f533fa813f1ac
Stijn Van de donk <stijnvandedonk@MBP-van-Stijn.fritz.box>
2018-10-26 13:53:41 +0200

    Start gemaakt aan Devices. Query, Device, Admin en User Class bijgewerkt. Tabel toegevoegd aan het Device Menu

--- src/GUI/DeviceMenu.java
+++ src/GUI/DeviceMenu.java
@@ -1,5 +1,8 @@
 package GUI;
 
+import logic_tier.User;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -27,6 +30,8 @@ import javax.swing.border.LineBorder;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import javax.swing.DefaultListCellRenderer;
 
 import java.awt.event.ItemListener;
@@ -39,10 +44,14 @@ import javax.swing.ComboBoxModel;
 public class DeviceMenu extends JFrame {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
+	User us;
 	private JPanel contentPane;
+	private DefaultTableModel model;
+	private JScrollPane scrollPane;
+	private JTable table;
 	private Color activeMenu = Color.decode("#43B7BA");
 	private Color hoverMenu = Color.decode("#ba8243");
 	private Color notActiveMenu = Color.decode("#cfa77a");
@@ -62,18 +71,18 @@ public class DeviceMenu extends JFrame {
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(0, 0, 350, 720);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JSeparator separator_5 = new JSeparator();
 		separator_5.setBounds(0, 193, 349, 12);
 		panel.add(separator_5);
-		
+
 		JButton btnNewButton = new JButton("Log Out");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
@@ -83,13 +92,13 @@ public class DeviceMenu extends JFrame {
 		});
 		btnNewButton.setBounds(77, 622, 190, 70);
 		panel.add(btnNewButton);
-		
+
 		JLabel lblDevices = new JLabel("Devices");
 		lblDevices.setForeground(activeMenu);
 		lblDevices.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblDevices.setBounds(6, 207, 190, 55);
 		panel.add(lblDevices);
-		
+
 		JLabel lblUsers = new JLabel("Users");
 		lblUsers.addMouseListener(new MouseAdapter() {
 			@Override
@@ -102,7 +111,7 @@ public class DeviceMenu extends JFrame {
 		lblUsers.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblUsers.setBounds(6, 267, 190, 55);
 		panel.add(lblUsers);
-		
+
 		JLabel lblSystemSettings = new JLabel("System Settings");
 		lblSystemSettings.addMouseListener(new MouseAdapter() {
 			@Override
@@ -115,14 +124,14 @@ public class DeviceMenu extends JFrame {
 		lblSystemSettings.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblSystemSettings.setBounds(6, 327, 300, 55);
 		panel.add(lblSystemSettings);
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_1.setBackground(Color.WHITE);
 		panel_1.setBounds(350, 0, 200, 720);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JButton btnNewDecicw = new JButton("New Device");
 		btnNewDecicw.addMouseListener(new MouseAdapter() {
 			@Override
@@ -134,69 +143,60 @@ public class DeviceMenu extends JFrame {
 		});
 		btnNewDecicw.setBounds(6, 622, 190, 70);
 		panel_1.add(btnNewDecicw);
-		
-		JList list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(6, 501, 190, -483);
-		panel_1.add(list);
-		
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 615, 190, -597);
+		panel_1.add(scrollPane);
+
 		JButton btnRemoveDevice = new JButton("Remove Device");
 		btnRemoveDevice.setBounds(1084, 622, 190, 70);
 		contentPane.add(btnRemoveDevice);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(549, 139, 725, 12);
 		contentPane.add(separator);
-		
+
 		JLabel lblNewLabel = new JLabel("Device 1");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblNewLabel.setBounds(601, 39, 277, 55);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblSettings = new JLabel("Settings");
 		lblSettings.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblSettings.setBounds(850, 152, 166, 55);
 		contentPane.add(lblSettings);
-		
+
 		JLabel lblTimer = new JLabel("Timer Status");
 		lblTimer.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblTimer.setBounds(601, 209, 277, 55);
 		contentPane.add(lblTimer);
-		
+
 		JLabel lblTimerOn = new JLabel("Timer On");
 		lblTimerOn.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblTimerOn.setBounds(601, 300, 277, 55);
 		contentPane.add(lblTimerOn);
-		
+
 		JLabel lblTimerOff = new JLabel("Timer Off");
 		lblTimerOff.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblTimerOff.setBounds(601, 393, 277, 55);
 		contentPane.add(lblTimerOff);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(579, 94, 363, 12);
 		contentPane.add(separator_1);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setBounds(579, 252, 310, 12);
 		contentPane.add(separator_2);
-		
+
 		JSeparator separator_3 = new JSeparator();
 		separator_3.setBounds(579, 348, 310, 12);
 		contentPane.add(separator_3);
-		
+
 		JSeparator separator_4 = new JSeparator();
 		separator_4.setBounds(579, 436, 310, 12);
 		contentPane.add(separator_4);
-		
+
 		Calendar startCalendar = Calendar.getInstance();
 		startCalendar.set(Calendar.HOUR_OF_DAY, 0);
 		startCalendar.set(Calendar.MINUTE, 0);
@@ -209,13 +209,13 @@ public class DeviceMenu extends JFrame {
         	startTime.addElement(startCalendar.getTime());
         	startCalendar.add(Calendar.MINUTE, 15);
         } while (startCalendar.getTime().before(startEnd.getTime()));
-		
+
 		JComboBox<Date> comboBoxTimeOn = new JComboBox<>(startTime);
 		comboBoxTimeOn.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
         comboBoxTimeOn.setRenderer(new DateFormattedListCellRenderer(new SimpleDateFormat("HH:mm")));
 		comboBoxTimeOn.setBounds(1020, 300, 161, 55);
 		contentPane.add(comboBoxTimeOn);
-		
+
 		Calendar endCalendar = Calendar.getInstance();
 		endCalendar.set(Calendar.HOUR_OF_DAY, 0);
 		endCalendar.set(Calendar.MINUTE, 0);
@@ -228,17 +228,17 @@ public class DeviceMenu extends JFrame {
         	endTime.addElement(endCalendar.getTime());
         	endCalendar.add(Calendar.MINUTE, 15);
         } while (endCalendar.getTime().before(endEnd.getTime()));
-		
+
 		JComboBox<Date> comboBoxTimeOff = new JComboBox<>(endTime);
 		comboBoxTimeOff.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
 		comboBoxTimeOff.setRenderer(new DateFormattedListCellRenderer(new SimpleDateFormat("HH:mm")));
 		comboBoxTimeOff.setBounds(1020, 393, 161, 55);
 		contentPane.add(comboBoxTimeOff);
-		
+
 		JButton btnSaveSettings = new JButton("Save Settings");
 		btnSaveSettings.setBounds(601, 622, 190, 70);
 		contentPane.add(btnSaveSettings);
-		
+
 		JToggleButton deviceTimerStatus = new JToggleButton("On");
 		deviceTimerStatus.setSelected(true);
 		deviceTimerStatus.addItemListener(new ItemListener() {
@@ -259,7 +259,7 @@ public class DeviceMenu extends JFrame {
 		});
 		deviceTimerStatus.setBounds(1020, 209, 161, 51);
 		contentPane.add(deviceTimerStatus);
-		
+
 		JToggleButton deviceEnableButton = new JToggleButton("On");
 		deviceEnableButton.setSelected(true);
 		deviceEnableButton.addItemListener(new ItemListener() {
@@ -283,9 +283,11 @@ public class DeviceMenu extends JFrame {
 		});
 		deviceEnableButton.setBounds(1020, 50, 161, 51);
 		contentPane.add(deviceEnableButton);
-		
+
+		createTable();
+
 	}
-	
+
 	public class DateFormattedListCellRenderer extends DefaultListCellRenderer {
 
         private DateFormat format;
@@ -303,4 +305,21 @@ public class DeviceMenu extends JFrame {
         }
 
     }
+
+	private void createTable() {
+	if (model != null)
+		model.setRowCount(0);
+	String[] header = { "Device Name" };
+	String[][] data = us.projects();
+	model = new DefaultTableModel(data, header);
+	table = new JTable(model) {
+		public boolean isCellEditable(int row, int column) {
+			return false;
+		}
+	};
+	table.setAutoResizeMode(table.AUTO_RESIZE_OFF);
+	table.getColumnModel().getColumn(0).setResizable(false);
+	table.getColumnModel().getColumn(0).setPreferredWidth(190);
+	scrollPane.setViewportView(table);
+}
 }
--- src/data_tier/QueryBuilder.java
+++ src/data_tier/QueryBuilder.java
@@ -1,9 +1,52 @@
 package data_tier;
 
+import java.sql.*;
+
 public class QueryBuilder {
 
+	private DatabaseManager dbm;
+
 	public QueryBuilder() {
-		// TODO Auto-generated constructor stub
+		dbm = new DatabaseManager();
+	}
+
+	// GET DEVICE DATA
+	public String setDeviceData(int deviceID) {
+		// Query invoegen
+		String query = "" + deviceID;
+		System.out.println(query);
+		return query;
+	}
+
+	//List devices
+	public ResultSet Devices() {
+		// Query invoegen
+		String query = "";
+		System.out.println(query);
+		return dbm.execute(query);
+	}
+
+	// Create new Device
+	public String insertNewDevice() {
+		// Query invoegen
+		String query = "";
+		System.out.println(query);
+		return query;
+	}
+
+	// Disble Device
+	public String disableDevice(Integer deviceID) {
+		// Query invoegen
+		String query = "XXXXXXXXXXX WHERE DeviceID=" + deviceID;
+		System.out.println(query);
+		return query;
+	}
+
+	// Remove Device
+	public String deleteDevice(Integer deviceID) {
+		String query = "DELETE FROM Devices WHERE DeviceID=" + deviceID;
+		System.out.println(query);
+		return query;
 	}
 
 }

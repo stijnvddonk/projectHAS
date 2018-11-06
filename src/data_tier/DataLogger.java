99edb0d1a7cfdf1bde49439cb8f3842317c70d3b
Stijn van de Donk <stijnvddonk@hotmail.com>
2018-11-06 11:30:14 +0100

    Log system uitgebreid

--- src/data_tier/DataLogger.java
+++ src/data_tier/DataLogger.java
@@ -7,11 +7,9 @@ import java.io.PrintWriter;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-import logic_tier.*;
-
 public class DataLogger {
 
-	//static String sessionName = User.sessionString();
+	//static String sessionName = Company.sessionString();
 	private static final String filename = logName() + ".txt";
 
 	public static String logName() {
@@ -28,13 +26,103 @@ public class DataLogger {
 		BufferedWriter bw = null;
 		PrintWriter out = null;
 		try {
-			file = new File("log/normal/" + name);
+			file = new File("log/system/" + name);
 			file.createNewFile();
 			if(file.exists()) {
 				fw = new FileWriter(file, true);
 				bw = new BufferedWriter(fw);
 				out = new PrintWriter(bw);
-				out.write(timeStamp + ": " + msg);
+				out.write(timeStamp + ": " + msg + "\n");
+				out.close();
+			}
+
+		} catch (Exception e) {
+			errorLog(e);
+		}
+	}
+
+	public static void deviceLog(Exception error) {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss ").format(new Date());
+		String name = filename;
+		File file = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter out = null;
+		try {
+			file = new File("log/device/" + name);
+			file.createNewFile();
+			if(file.exists()) {
+				fw = new FileWriter(file, true);
+				bw = new BufferedWriter(fw);
+				out = new PrintWriter(bw);
+				out.write(timeStamp + ": " + error + "\n");
+				out.close();
+			}
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
+
+	public static void userLog(Exception error) {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss ").format(new Date());
+		String name = filename;
+		File file = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter out = null;
+		try {
+			file = new File("log/user/" + name);
+			file.createNewFile();
+			if(file.exists()) {
+				fw = new FileWriter(file, true);
+				bw = new BufferedWriter(fw);
+				out = new PrintWriter(bw);
+				out.write(timeStamp + ": " + error + "\n");
+				out.close();
+			}
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
+
+	public static void apiLog(String msg) {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss ").format(new Date());
+		String name = filename;
+		File file = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter out = null;
+		try {
+			file = new File("log/api/" + name);
+			file.createNewFile();
+			if(file.exists()) {
+				fw = new FileWriter(file, true);
+				bw = new BufferedWriter(fw);
+				out = new PrintWriter(bw);
+				out.write(timeStamp + ": " + msg + "\n");
+				out.close();
+			}
+
+		} catch (Exception e) {
+			errorLog(e);
+		}
+	}
+
+	public static void serverLog(String msg) {
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss ").format(new Date());
+		String name = filename;
+		File file = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		PrintWriter out = null;
+		try {
+			file = new File("log/server/" + name);
+			file.createNewFile();
+			if(file.exists()) {
+				fw = new FileWriter(file, true);
+				bw = new BufferedWriter(fw);
+				out = new PrintWriter(bw);
+				out.write(timeStamp + ": " + msg + "\n");
 				out.close();
 			}
 
@@ -51,17 +139,17 @@ public class DataLogger {
 		BufferedWriter bw = null;
 		PrintWriter out = null;
 		try {
-			file = new File("log/Error/" + name);
+			file = new File("log/error/" + name);
 			file.createNewFile();
 			if(file.exists()) {
 				fw = new FileWriter(file, true);
 				bw = new BufferedWriter(fw);
 				out = new PrintWriter(bw);
-				out.write(timeStamp + ": " + error);
+				out.write(timeStamp + ": " + error + "\n");
 				out.close();
 			}
 		} catch (Exception e) {
-			DataLogger.log("error\n");
+			System.out.println("error");
 		}
 	}
 }
